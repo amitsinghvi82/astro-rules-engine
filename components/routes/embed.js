f52c57d927e55bbe805ef8093cc3c9c992c37530
amitsinghvi82 <46282717+amitsinghvi82@users.noreply.github.com>
2019-01-07 21:14:42 +0530

    Routes

--- components/routes/embed.js
+++ components/routes/embed.js
@@ -1,23 +1,42 @@
-module.exports = function(webserver, controller) {
+var sdkClient = require('../../sdk/sdk');
 
+// make some dummy data in order to call vedic rishi api
+var data = {
+  'date': 10,
+  'month': 12,
+  'year': 1993,
+  'hour': 1,
+  'minute': 25,
+  'latitude': 25,
+  'longitude': 82,
+  'timezone': 5.5
+};
 
-// This creates the /embed route, where an easy-to-copy embed code is available
-webserver.get('/embed', function(req,res) {
+// api name which is to be called
+var resource = "horo_chart/D1";
 
-    res.render('embed', {
-      layout: 'layouts/default',
-      base_url: req.hostname
-    });
+module.exports = function(webserver) {
 
-});
-
-// This creates the /embed route, where an easy-to-copy embed code is available
 webserver.get('/', function(req,res) {
+  // call horoscope apis
+  sdkClient.call(resource, data.date, data.month, data.year, data.hour, data.minute, data.latitude, data.longitude, data.timezone, function(error, result){
 
-    res.render('index', {
-      layout: 'layouts/default',
-      base_url: req.hostname
-    });
+  if(error)
+  {
+      console.log("Error returned!!");
+  }
+  else
+  {
+      console.log('Response has arrived from API server --');
+      console.log(result);
+      res.render('index', {
+        layout: 'layouts/default',
+        base_url: req.hostname,
+        api_response: result
+      });
+  }
+  });
+    
 
 });
 
