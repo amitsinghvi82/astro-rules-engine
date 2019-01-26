c391c35f847a08ccce450f45bea2846ca1b4a52b
amitsinghvi82 <46282717+amitsinghvi82@users.noreply.github.com>
2019-01-26 14:05:49 +0530

    Rules

---
+++ rules/lord.js
@@ -0,0 +1,16 @@
+var rashiLord = {
+    "Aries":"Mars",
+    "Taurus":"Venus",
+    "Gemini":"Mercury",
+    "Cancer":"Moon",
+    "Leo":"Sun",
+    "Virgo":"Mercury",
+    "Libra":"Venus",
+    "Scorpius":"Mars",
+    "Sagittarius":"Jupiter",
+    "Capricornus":"Saturn",
+    "Aquarius":"Saturn",
+    "Pisces":"Jupiter"
+}
+
+module.exports = rashiLord;
