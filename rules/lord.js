e69f26cd0c6f5a489f501906eadd0c8fca6d3971
amitsinghvi82 <46282717+amitsinghvi82@users.noreply.github.com>
2019-01-27 08:14:15 +0530

    Rules

--- rules/lord.js
+++ rules/lord.js
@@ -1,16 +1,23 @@
 var rashiLord = {
-    "Aries":"Mars",
-    "Taurus":"Venus",
-    "Gemini":"Mercury",
-    "Cancer":"Moon",
-    "Leo":"Sun",
-    "Virgo":"Mercury",
-    "Libra":"Venus",
-    "Scorpius":"Mars",
-    "Sagittarius":"Jupiter",
-    "Capricornus":"Saturn",
-    "Aquarius":"Saturn",
-    "Pisces":"Jupiter"
+    1:"Ma",
+    2:"Ve",
+    3:"Me",
+    4:"Mo",
+    5:"Su",
+    6:"Me",
+    7:"Ve",
+    8:"Ma",
+    9:"Ju",
+    10:"Sa",
+    11:"Sa",
+    12:"Ju"
 }
 
-module.exports = rashiLord;
+var dhanLord = {
+	
+	getDhanLord: (sign) => {		
+		return rashiLord[sign];
+    }
+}
+
+module.exports = dhanLord;
