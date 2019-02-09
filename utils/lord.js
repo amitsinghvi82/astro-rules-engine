82e390b53a620a0ccc3ffed0d4293cc015d33cb8
amitsinghvi82 <46282717+amitsinghvi82@users.noreply.github.com>
2019-02-09 19:25:26 +0530

    Utility methods

--- utils/lord.js
+++ utils/lord.js
@@ -19,7 +19,7 @@ var dhanLord = {
 		return rashiLord[sign];
     },
 
-    getDhanLordMoonDistance: (response, dhanLord) => {        
+    getMoonToDhanLordDistance: (response, dhanLord) => {        
         var dhanLordPosition;
         var moonPosition;		
 		response.forEach(function(house) {
@@ -32,8 +32,13 @@ var dhanLord = {
                     moonPosition = house.sign;
                 }
             })            
-        });        
-        return Math.abs(dhanLordPosition - moonPosition + 1);
+        });
+        if (dhanLordPosition >= moonPosition) {
+            return Math.abs(dhanLordPosition - moonPosition + 1);
+        } else {
+            return Math.abs(12 - moonPosition + 1 + dhanLordPosition);
+        }       
+        
     },
 
     getMarsMoonDistance: (response) => {        
@@ -49,8 +54,29 @@ var dhanLord = {
                     moonPosition = house.sign;
                 }
             })            
-        });        
-        return Math.abs(marsPosition - moonPosition + 1);
+        });
+        if (moonPosition >= marsPosition ) {
+            return Math.abs(moonPosition - marsPosition + 1);
+        } else {
+            return Math.abs(12 - marsPosition + 1 + moonPosition );
+        }                
+    },
+
+    isInterchangeRuleValid: (birthResponse, currentResponse) => {
+        var birthLords = [rashiLord[birthResponse[0].sign],rashiLord[birthResponse[1].sign],rashiLord[birthResponse[4].sign],rashiLord[birthResponse[6].sign],rashiLord[birthResponse[8].sign],rashiLord[birthResponse[10].sign]];
+        var currentLords = [rashiLord[currentResponse[0].sign],rashiLord[currentResponse[1].sign],rashiLord[currentResponse[4].sign],rashiLord[currentResponse[6].sign],rashiLord[currentResponse[8].sign],rashiLord[currentResponse[10].sign]];
+        console.log('Birth Lords:' + birthLords);
+        console.log('Current Lords:' + currentLords);
+        for (let i = 0; i < birthLords.length; i++) {
+            for (let j = 0; j < currentLords.length; j++) {
+                if (i == j) continue;
+                if (birthLords[i] == currentLords[j] && birthLords[j] == currentLords[i]) {
+                    console.log('Birth Lord: ' + birthLords[i] + ' interchanged with Current lord: ' + currentLords[i]);
+                    return true;
+                } 
+            }            
+        }
+        return false;
     },
 
     getJupiterMoonDistance: (response) => {        
@@ -66,9 +92,15 @@ var dhanLord = {
                     moonPosition = house.sign;
                 }
             })            
-        });        
-        return Math.abs(jupiterPosition - moonPosition + 1);
+        });
+        if (moonPosition >= jupiterPosition ) {
+            return Math.abs(moonPosition - jupiterPosition + 1);
+        } else {
+            return Math.abs(12 - jupiterPosition + 1 + moonPosition );
+        }                        
     }
+
+    
 }
 
 module.exports = dhanLord;
